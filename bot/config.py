af36ba9b692cbc22f64571952e38beca52c66f9a
leechwork3-pixel <leechwork3@gmail.com>
2025-07-31 15:24:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram Bot API credentials from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "24171111"))
+API_HASH = os.environ.get("API_HASH", "c850cb56b64b6c3b10ade9c28ef7966a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7804274444:AAESKpYJQVhftykvv5cKZP2uyCYvxlwQvow")
 
 # MongoDB Database URI
 MONGO_URI = os.environ.get("MONGO_URI", "")
@@ -16,11 +16,11 @@ DB_NAME = os.environ.get("DB_NAME", "EbookConverterBot")
 
 # Sudo Admins: User IDs of bot owners who can grant/revoke admin rights.
 # Add your own Telegram user ID here.
-SUDO_ADMINS = [int(admin_id) for admin_id in os.environ.get("SUDO_ADMINS", "123456789").split()]
+SUDO_ADMINS = [int(admin_id) for admin_id in os.environ.get("SUDO_ADMINS", "1335306418").split()]
 
 # Log Channel: A public/private channel ID to send logs and new user notifications.
 # Make sure your bot is an admin in this channel.
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0")) # Example: -1001234567890
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002585029413")) # Example: -1001234567890
 
 # Telegraph account name
 TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "ImageUploader")
